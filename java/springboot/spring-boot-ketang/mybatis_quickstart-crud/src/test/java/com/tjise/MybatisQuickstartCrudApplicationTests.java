d33a775966d1bd612379b899b9cc11ec1b2a86bc
Swot <swotpp@163.com>
2025-09-15 17:07:10 +0800

    feat(SpringBoot): 课堂进度更新

--- java/springboot/spring-boot-ketang/mybatis_quickstart-crud/src/test/java/com/tjise/MybatisQuickstartCrudApplicationTests.java
+++ java/springboot/spring-boot-ketang/mybatis_quickstart-crud/src/test/java/com/tjise/MybatisQuickstartCrudApplicationTests.java
@@ -1,19 +1,20 @@
 //@+leo-ver=5-thin
-//@+node:swot.20241016164835.1: * @file src/test/java/com/tjise/MybatisQuickstartCrudApplicationTests.java
+//@+node:swot.20250915162444.1: * @file src/test/java/com/tjise/MybatisQuickstartCrudApplicationTests.java
 //@@language java
 //@+doc
-// 接下来，我们就可以直接在单元测试类中通过 @Autowired 注解 EmpMapper 接口。
-// 然后就可以直接调用其 deleteEmpById 方法传递参数进行测试了。
-//
 // [source,java,linenums]
 // ----
 //@@c
 package com.tjise;
 
 import com.tjise.mapper.EmpMapper;
+import com.tjise.pojo.Emp;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 
 @SpringBootTest
@@ -22,14 +23,69 @@ class MybatisQuickstartCrudApplicationTests {
     @Autowired
     private EmpMapper empMapper;
 
+    // 省略其他代码显示
+    //@+others
+    //@+node:swot.20250915162516.1: ** @ignore-node delete test
     @Test
     void deleteEmpByIdTest() {
-        empMapper.deleteEmpById(15);
+        empMapper.deleteEmpById(17);
+    }
+
+    @Test
+    void deleteFromTableByIdTest() {
+        empMapper.deleteFromTableById("emp");
+    }
+    //@+node:swot.20250915162835.1: ** @ignore-node insertEmpTest
+    @Test
+    public void insertEmpTest(){
+        // 创建实体类对象
+        Emp emp = new Emp();
+        emp.setUsername("zhouyuan");
+        emp.setName("周元");
+        emp.setGender((short) 2);
+        emp.setImage("2.png");
+        emp.setJob((short) 2);      // 讲师
+        emp.setEntrydate(LocalDate.of(2003,2,10));
+        emp.setDeptId(2);   // 先随便给个数值吧
+        emp.setCreateTime(LocalDateTime.now());
+        emp.setUpdateTime(LocalDateTime.now());
+
+        empMapper.insertEmp(emp);
+        System.out.println("emp.id: " + emp.getId());
+    }
+    //@+node:swot.20250915162847.1: ** @ignore-node updateEmpTest
+    @Test
+    public void updateEmpTest(){
+        // 创建实体类对象
+        Emp emp = new Emp();
+        emp.setId(13);
+        emp.setUsername("fanghan");
+        emp.setName("方寒");
+        emp.setGender((short) 2);
+        emp.setImage("2.png");
+        emp.setJob((short) 2);      // 讲师
+        emp.setEntrydate(LocalDate.of(2003,2,10));
+        emp.setDeptId(2);   // 先随便给个数值吧
+        emp.setUpdateTime(LocalDateTime.now());
+
+        empMapper.updateEmp(emp);
+    }
+    //@-others
+
+    @Test
+    public void selectEmpByIdTest() {
+        Emp emp = empMapper.selectEmpById(13);
+        System.out.println(emp);  // 有的字段封装失败
     }
 
 }
 //@+doc
 // ----
 //
-// 测试结果为删除了指定的数据库记录。
+// .控制台打印更新记录成功
+// ....
+// ==>  Preparing: update emp set username=?, name=?, gender=?, image=?, job=?, entrydate=?, dept_id=?, update_time=? where id=?
+// ==> Parameters: fanghan(String), 方寒(String), 2(Short), 2.png(String), 2(Short), 2003-02-10(LocalDate), 2(Integer), 2024-10-19T20:05:23.088(LocalDateTime), 13(Integer)
+// <==    Updates: 1
+// ....
 //@-leo
