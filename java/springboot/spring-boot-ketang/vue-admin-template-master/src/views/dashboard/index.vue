ee46a030e11252b09bd90ce3528bddfb34b59afd
Swot <swotpp@163.com>
2025-09-23 15:14:52 +0800

    feat(vue2): First upload

---
+++ java/springboot/spring-boot-ketang/vue-admin-template-master/src/views/dashboard/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="dashboard-container">
+     <img src="@/assets/welcome.png" width="100%" height="100%">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Dashboard',
+  
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
