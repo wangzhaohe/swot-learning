81b2f282b56596b323f4e183ccb9a4b77bf36849
Swot <swotpp@163.com>
2025-09-24 10:59:56 +0800

    feat(SpringBoot): Add yaml

--- java/springboot/spring-boot-ketang/general-manager/src/main/java/com/tjise/utils/AliOSSUtils.java
+++ java/springboot/spring-boot-ketang/general-manager/src/main/java/com/tjise/utils/AliOSSUtils.java
@@ -1,32 +1,47 @@
 //@+leo-ver=5-thin
-//@+node:swot.20250814205415.1: * @file src/main/java/com/tjise/utils/AliOSSUtils.java
+//@+node:swot.20250924105814.1: * @file src/main/java/com/tjise/utils/AliOSSUtils.java
 //@@language java
 //@+doc
-// [source,java]
+// [source,java,linenums]
 // ----
 //@@c
 package com.tjise.utils;
 
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
-import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-// 阿里云 OSS 工具类
-// 放入 IOC 容器中，不属于 控制层@Controller、业务层@Service、持久层@Repository，所以就用 @Component 吧
+/**
+ * 阿里云 OSS 工具类
+ */
+ 
+// 放入IOC 容器中，不属于 控制层@Controller、业务层@Service、持久层@Repository，所以就用 @Component 吧
 @Component
-@Data
 public class AliOSSUtils {
-    // 这儿的参数是写死的，后面再解决
-    private String endpoint = "https://oss-cn-beijing.aliyuncs.com";
-    private String accessKeyId = "[REMOVED]";
-    private String accessKeySecret = "[REMOVED]";
-    private String bucketName = "swot-learn";
-    // 实现上传图片到OSS
+    // 这儿放置 @Value 取值
+    //@+others
+    //@+node:swot.20250924105814.2: ** 原来下面 4 个参数值是写死的，现在使用 @Value 读取配置
+    // @Value 注解通常用于外部配置的属性注入，具体用法为： @Value("${配置文件中的key}")
+    @Value("${aliyun.oss.endpoint}")
+    private String endpoint;
+
+    @Value("${aliyun.oss.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${aliyun.oss.accessKeySecret}")
+    private String accessKeySecret;
+
+    @Value("${aliyun.oss.bucketName}")
+    private String bucketName;
+    //@-others
+    /**
+     * 实现上传图片到OSS
+     */
     public String upload(MultipartFile multipartFile) throws IOException {
         // 获取上传的文件的输入流
         InputStream inputStream = multipartFile.getInputStream();
@@ -41,8 +56,9 @@ public class AliOSSUtils {
 
         //文件访问路径
         String url = endpoint.split("//")[0] + "//" + bucketName + "." + endpoint.split("//")[1] + "/" + fileName;
-        ossClient.shutdown();  // 关闭 ossClient
-        return url;            // 把上传到 oss 的路径返回
+        // 关闭ossClient
+        ossClient.shutdown();
+        return url;// 把上传到oss的路径返回
     }
 }
 //@+doc
