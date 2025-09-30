6394e9a13d60ccd96cbf79a5d1ea9489734a52f5
Swot <swotpp@163.com>
2025-09-30 15:03:06 +0800

    feat(SpringBoot): Add Redis 数据库应用

--- java/springboot/spring-boot-ketang/general-manager/src/main/java/com/tjise/controller/LoginController.java
+++ java/springboot/spring-boot-ketang/general-manager/src/main/java/com/tjise/controller/LoginController.java
@@ -1,12 +1,12 @@
 //@+leo-ver=5-thin
-//@+node:swot.20250816215207.1: * @file src/main/java/com/tjise/controller/LoginController.java
+//@+node:swot.20250907160742.1: * @file src/main/java/com/tjise/controller/LoginController.java
 //@@language java
 //@+doc
 // [source,java]
 // ----
 //@@c
 //@+others
-//@+node:swot.20250907113010.1: ** @ignore-node import
+//@+node:swot.20250907160742.2: ** @ignore-node import
 package com.tjise.controller;
 
 import com.tjise.pojo.Emp;
@@ -14,18 +14,35 @@ import com.tjise.pojo.Result;
 import com.tjise.service.EmpService;
 import com.tjise.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 //@-others
 @RestController
 public class LoginController {
 
     @Autowired
     private EmpService empService;
+
+    //@+<< 注入 redis 对象 >>
+    //@+node:swot.20250907161802.1: ** << 注入 redis 对象 >>
+    //@+doc
+    // [source,java]
+    // ----
+    //@@c
+    //@@language java
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    //@+doc
+    // ----
+    //
+    //@-<< 注入 redis 对象 >>
 
     @PostMapping("/login")
     public Result login(@RequestBody Emp emp) {
@@ -44,13 +61,28 @@ public class LoginController {
 
             // 生成令牌并返回给前端
             String jwt = JwtUtils.generateJwt(map);
+
+            //@+<< 将 jwt token 存入 redis 数据库 >>
+            //@+node:swot.20250907162203.1: ** << 将 jwt token 存入 redis 数据库 >>
+            //@+doc
+            // [source,java]
+            // ----
+            //@@c
+            //@@language java
+            stringRedisTemplate.opsForValue().set(
+                    e.getUsername(),       // key，将用户名 username 存入 redis
+                    jwt,                   // value，将 token 存入 redis
+                    JwtUtils.expire,       // 过期时间
+                    TimeUnit.MILLISECONDS  // 时间单位
+            );
+            //@+doc
+            // ----
+            //@-<< 将 jwt token 存入 redis 数据库 >>
+
             return Result.success(jwt);
         }
     }
 }
 //@+doc
 // ----
-//
-// 使用 httpie 测试登录::    
-// http -v post http://localhost:8080/login username=limuwan password=123456
 //@-leo
--- java/springboot/spring-boot-ketang/general-manager/src/main/java/com/tjise/interceptor/LoginCheckedInterceptor.java
+++ java/springboot/spring-boot-ketang/general-manager/src/main/java/com/tjise/interceptor/LoginCheckedInterceptor.java
@@ -1,13 +1,17 @@
 //@+leo-ver=5-thin
-//@+node:swot.20250819062254.1: * @file src/main/java/com/tjise/interceptor/LoginCheckedInterceptor.java
+//@+node:swot.20250907212631.1: * @file src/main/java/com/tjise/interceptor/LoginCheckedInterceptor.java
 //@@language java
 //@+others
-//@+node:swot.20250831211100.1: ** @ignore-node import
+//@+node:swot.20250907212631.2: ** @ignore-node import
+//@@language java
 package com.tjise.interceptor;
 
 import com.alibaba.fastjson2.JSONObject;
 import com.tjise.pojo.Result;
 import com.tjise.utils.JwtUtils;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,90 +19,87 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-//@+node:swot.20250831211132.1: ** @ignore-node class LoginCheckedInterceptor
+//@+node:swot.20250907212631.3: ** @ignore-node class LoginCheckedInterceptor 前端请求 token 检查拦截器
 //@+doc
 // [source,java]
 // ----
 //@@c
-/**
- * 登录检查拦截器
- */
 @Component
-public class LoginCheckedInterceptor implements HandlerInterceptor {
+public class LoginCheckedInterceptor implements HandlerInterceptor {  // <1>
     //@+others
-    //@+node:swot.20250831210446.1: *3* override preHandle -> 增加处理 CORS 预检请求 OPTIONS
+    //@+node:swot.20250907222456.1: *3* 注入 redis 对象
     //@+doc
     // [source,java]
     // ----
     //@@c
     //@@language java
-    /**
-    * 目标 api 执行前执行: 
-    *    return true 放行
-    *    return false 拦截
-    */
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    //@+doc
+    // ----
+    //
+    //@+node:swot.20250907212631.4: *3* preHandle 目标 api 执行前执行: return true 放行，return false 拦截
+    //@+doc
+    // [source,java]
+    // ----
+    //@@c
+    //@@language java
     @Override
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response,
                              Object handler) throws Exception {
-        System.out.println(
-            "[INTERCEPTOR] " + java.time.LocalDateTime.now() + " - Enter preHandle()");
+        System.out.println("Enter preHandle()");
 
-        // 增加处理 CORS 预检请求，这个要加，不加会报错的
-        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-            System.out.println(
-                "[INTERCEPTOR] " + java.time.LocalDateTime.now() + " - Handling OPTIONS request");
-            response.setStatus(HttpServletResponse.SC_OK);
-            return true;
-        }
-        // 其他拦截逻辑
-        //@+others
-        //@+node:swot.20250831211507.1: *4* @ignore-node 其他拦截逻辑
         // 1. 获取请求路由
         String url = request.getRequestURL().toString();
 
         // 2. 判断请求的路由是否包含 login
         if (url.contains("login")) {
-            // 放行去访问 login api
-            return true;
+            return true;  // 放行去访问 login api
         }
 
         // 3. 获取请求头中的令牌 token
         String jwt = request.getHeader("token");
         if (jwt == null) {  // 返回登录页面
             sendErrorResponse(response);
-            return false;
+            return false;  // 拦截直接给客户端响应 200 结束
         }
-
+        // ---------- 更新代码开始 ------
         // 4. 解析令牌
+        Claims payload;
         try {
-            JwtUtils.parseJWT(jwt);
+            payload = JwtUtils.parseJWT(jwt);
         } catch (Exception e) {
             sendErrorResponse(response);
             return false;
-        }   
+        }
 
+        // 5. 对比 redis 数据库中的值
+              // 如果删除 redis 中用户的键，则能将用户踢出系统
+        String username = (String) payload.get("username");
+        String tokenFromRedis = stringRedisTemplate.opsForValue()
+                                                   .get(username);
+        // 如果为 null 说明该用户不在线（如未注册、key过期、被删除踢下线）
+        // tokenFromRedis 与 jwt 不相等，说明 jwt 提供的是非法用户
+        if (tokenFromRedis == null || !tokenFromRedis.equals(jwt)) {
+            sendErrorResponse(response);
+            return false;
+        }
+        // ---------- 更新代码结束 ------
         return true;   // 放行去执行 api
-        // return false;  // 拦截直给客户端响应 200 结束
-        //@-others
     }
     //@+doc
     // ----
-    //@+node:swot.20250831210552.1: *3* @ignore-tree
-    //@+others
-    //@+node:swot.20250831210440.1: *4* override postHandle
-    // 目标 api 执行后执行
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.println("Enter postHandle()");
-    }
-    //@+node:swot.20250831210435.1: *4* override afterCompletion
-    // 请求处理后执行
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        System.out.println("Enter afterCompletion()");
-    }
-    //@+node:swot.20250819084555.1: *4* private void sendErrorResponse
+    //
+    // .使用 httpie 测试，查看后端日志中打印结果
+    // [source,console]
+    // ----
+    // TOKEN=$(http POST :8080/login username=limuwan password=123456 | jq -r '.data')
+    // http :8080/depts "token: $TOKEN"
+    // http :8080/depts/1 "token: $TOKEN"
+    // ----
+    //
+    //@+node:swot.20250907212631.5: *3* @ignore-node sendErrorResponse() 自定义发送错误响应方法
     //@@language java
     //@+doc
     // [source,java]
@@ -107,11 +108,11 @@ public class LoginCheckedInterceptor implements HandlerInterceptor {
     // 抽取的私有方法
     private void sendErrorResponse(HttpServletResponse response) throws IOException {
         Result notLogin = Result.error("NOT_LOGIN");
-        // return notLogin;  // 行不通，因为 doFilter 返回值只能是 void
+        // return notLogin;  // 行不通，因为 preHandle 返回值只能是 boolean
         // 所以可以使用 response.getWriter().write() 给前端返回约定好的 json 格式的数据
         String jsonString = JSONObject.toJSONString(notLogin);
         // response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);  // 这个不要设置，会返回401，前端案例没处理
-        response.setContentType("application/json;charset=UTF-8");
+        response.setContentType("application/json;charset=UTF-8");  // 防止中文乱码
         response.getWriter().write(jsonString);
 
         // 对于不复杂场景，直接返回固定的 JSON 字符串更简单
@@ -121,11 +122,30 @@ public class LoginCheckedInterceptor implements HandlerInterceptor {
     }
     //@+doc
     // ----
-    //
-    //@-others
+    //@+node:swot.20250907212631.6: *3* @ignore-node postHandle 目标 api 执行后执行：了解
+    @Override
+    public void postHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler, ModelAndView modelAndView) throws Exception
+    {
+        System.out.println("Enter postHandle()");
+    }
+    //@+node:swot.20250907212631.7: *3* @ignore-node afterCompletion 请求处理后执行：了解
+    @Override
+    public void afterCompletion(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler, Exception ex) throws Exception
+    {
+        System.out.println("Enter afterCompletion()");
+    }
     //@-others
 }
 //@+doc
 // ----
+//
+// <1> 实现接口 HandlerInterceptor
+//
 //@-others
 //@-leo
