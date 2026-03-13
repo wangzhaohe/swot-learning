4d69673342a705a8dacfa9365cf3b1ca194d233f
Swot <swotpp@163.com>
2026-03-13 15:15:05 +0800

    docs(java): Move HashMapExample

---
+++ 大软3+2/Java高级程序设计进阶/code/testBasicSyntax/src/HashMapExample/DoorException.java
@@ -0,0 +1,11 @@
+// @clean code/testBasicSyntax/src/ExceptionSelfdefineExample/DoorException.java
+package HashMapExample;
+
+/**
+ * 自定义异常：专门处理门禁系统的异常情况
+ */
+public class DoorException extends Exception {
+    public DoorException(String message) {
+        super(message); // 把错误信息传给父类去处理
+    }
+}
