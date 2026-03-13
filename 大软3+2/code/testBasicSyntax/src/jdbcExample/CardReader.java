f98d2150462cb4adb2d90de922edbe70525eb63a
Swot <swotpp@163.com>
2026-03-13 15:19:48 +0800

    chroe: Delete moved files

--- 大软3+2/code/testBasicSyntax/src/jdbcExample/CardReader.java
+++
@@ -1,9 +0,0 @@
-// @clean code/testBasicSyntax/src/jdbcExample/CardReader.java
-package jdbcExample;
-
-class CardReader {
-    public String readPhysicalSignal(String simulatedInput) {
-        System.out.println("[硬件层] 传感器感应到物理信号...");
-        return simulatedInput;
-    }
-}
