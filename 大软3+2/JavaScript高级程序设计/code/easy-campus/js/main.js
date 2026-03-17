e4017b91b049bf8fdb550cbcfe1d5a2372a5b3aa
Swot <swotpp@163.com>
2026-03-17 11:22:19 +0800

    docs(js): Add Ch 2 更新: 业务逻辑 2026-03-17

--- 大软3+2/JavaScript高级程序设计/code/easy-campus/js/main.js
+++ 大软3+2/JavaScript高级程序设计/code/easy-campus/js/main.js
@@ -3,6 +3,7 @@
 //@@language javascript
 //@+others
 //@+node:swot.20260316162901.1: ** Ch 1 更新: 初始化 2026-03-16
+console.log('Ch 1 更新: 初始化 2026-03-16');
 //@+node:swot.20260314094048.1: *3* BB JSDoc
 /**
  * 文件名：main.js
@@ -37,5 +38,43 @@ currentMoney = currentMoney + 100;
 
 //@+node:swot.20260314094123.1: *3* BB 再次输出信息
 console.log(`充值成功！当前余额：${currentMoney} 元`);
+//@+node:swot.20260317110936.1: ** Ch 2 更新: 业务逻辑 2026-03-17
+console.log('Ch 2 更新: 业务逻辑 2026-03-17');
+//@+node:swot.20260314142703.1: *3* BB 增加用户级别
+//@@language javascript
+let isVIP = true;      // 是否是VIP会员
+
+let vipLevel = 1;      // VIP等级 1:普通会员 2:超级会员
+//@+node:swot.20260314142957.1: *3* BB 商品折扣计算器
+//@@language javascript
+// 定义商品原价
+let price = 100;
+
+// 计算逻辑
+let finalPrice = price; // 默认原价
+
+if (vipLevel === 2) {
+    finalPrice = price * 0.8; // 8折
+    console.log("尊贵的超级会员，您享受8折优惠！");
+}
+else if (vipLevel === 1) {
+    finalPrice = price * 0.9; // 9折
+    console.log("尊敬的会员，您享受9折优惠！");
+}
+else {
+    console.log("您暂无折扣，建议开通会员省钱哦~");
+}
+
+console.log(`原价：${price}元，折后价：${finalPrice}元`);
+//@+node:swot.20260314143438.1: *3* BB 登录身份验证
+//@@language javascript
+// 利用逻辑运算符 &&
+if (isVIP && currentMoney > 0) {
+    console.log("验证通过！欢迎进入会员专属区！");
+    // 这里后续会跳转页面，现在只打印日志
+}
+else {
+    console.log("您不是会员或余额不足，无法访问该板块！");
+}
 //@-others
 //@-leo
