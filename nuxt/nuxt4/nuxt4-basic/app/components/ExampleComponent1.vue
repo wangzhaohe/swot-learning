41407acc276a3c8b99088085bd996ddd585f2243
Swot <swotpp@163.com>
2026-05-13 15:08:07 +0800

    docs(nuxt4): Add Components

---
+++ nuxt/nuxt4/nuxt4-basic/app/components/ExampleComponent1.vue
@@ -0,0 +1,12 @@
+//@+leo-ver=5-thin
+//@+node:swot.20260513143454.1: * @file app/components/ExampleComponent1.vue
+//@@language javascript
+//@+<< template Component1 >>
+//@+node:swot.20260513143523.1: ** << template Component1 >>
+<template>
+    <div>
+        This is  an Example Component1
+    </div>
+</template>
+//@-<< template Component1 >>
+//@-leo
---
+++ nuxt/nuxt4/nuxt4-basic/app/pages/UseExampleComponent1.vue
@@ -0,0 +1,13 @@
+//@+leo-ver=5-thin
+//@+node:swot.20260513143723.1: * @file app/pages/UseExampleComponent1.vue
+//@@language javascript
+//@+<< template use ExampleComponent1 >>
+//@+node:swot.20260513143729.1: ** << template use ExampleComponent1 >>
+<template>
+    <div>
+        <!--直接使用即可，不用导入-->
+        <ExampleComponent1 />
+    </div>
+</template>
+//@-<< template use ExampleComponent1 >>
+//@-leo
