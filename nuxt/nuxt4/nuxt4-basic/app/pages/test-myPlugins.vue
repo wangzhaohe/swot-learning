b45cd98dfb82642eb3860db9a18b6ea0ae5f3721
Swot <swotpp@163.com>
2026-05-15 16:28:15 +0800

    docs(nuxt4): Add plugins

---
+++ nuxt/nuxt4/nuxt4-basic/app/pages/test-myPlugins.vue
@@ -0,0 +1,21 @@
+//@+leo-ver=5-thin
+//@+node:swot.20260515143609.1: * @file app/pages/test-myPlugins.vue
+//@@language javascript
+//@+<< pages test-myPlugins >>
+//@+node:swot.20260515143729.1: ** << pages test-myPlugins >>
+//@@language pug
+<template lang="pug">
+//- 模板中可以直接使用 $hello
+div
+    h2 test-myPlugins.vue
+    div {{ $hello('world') }}
+</template>
+
+//@@language typescript
+<script setup>
+// 也可以在 script 中使用 $hello
+const { $hello } = useNuxtApp();
+console.log($hello('world'));
+</script>
+//@-<< pages test-myPlugins >>
+//@-leo
---
+++ nuxt/nuxt4/nuxt4-basic/app/plugins/myPlugin.ts
@@ -0,0 +1,21 @@
+//@+leo-ver=5-thin
+//@+node:swot.20260515143229.1: * @file app/plugins/myPlugin.ts
+//@@language javascript
+//@+<< defineNuxtPlugin >>
+//@+node:swot.20260515143229.2: ** << defineNuxtPlugin >>
+//@@language javascript
+export default defineNuxtPlugin(
+    nuxtApp => {
+        console.log("nuxtApp", nuxtApp);
+
+        // Automatically Providing Helpers
+        return {
+            provide: {
+                hello: (msg: string) => `Hello ${msg}`
+            }
+        }
+        // 具体使用参下面 test-plugins.vue
+    }
+);
+//@-<< defineNuxtPlugin >>
+//@-leo
