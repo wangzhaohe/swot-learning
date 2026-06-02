724e6a1c1a5b075d87433fe337182dc0bdc958ae
Swot <swotpp@163.com>
2026-06-02 10:51:45 +0800

    feat(微信小程序): 只实现了一个新闻项

--- 大软3+2/miniprogram/code/pages/posts/posts.js
+++ 大软3+2/miniprogram/code/pages/posts/posts.js
@@ -1,18 +1,26 @@
-// pages/register/register.js
+// pages/posts/posts.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.setData({
+      avatar: "https://i.pravatar.cc/150?img=1",
+      publishTime: "2025-12-12 12:12:12",
+      title: "2026 LPL 夏季赛季后赛观赛指南",
+      imageUrl :"https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?w=750&h=300&fit=crop",
+      desc: "这里是新闻简介。这里是新闻简介这里是新闻简介这里是新闻简介这里是新闻简介这里是新闻简介这里是新闻简介这里是新闻简介这里是新闻简介这里是新闻简介这里是新闻简介这里是新闻简介这里是新闻简介",
+      likeCount: 20,
+      viewCount: 100
+    })
   },
 
   /**
