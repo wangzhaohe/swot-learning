5999ab09c798fbb782161860766f5b7fc658320c
wangzhaohe <swotpp@163.com>
2026-09-14 15:24:40 +0800

    feat(svelte): Add a new page

--- svelte-sveltekit/svelte-learning/src/routes/+page.svelte
+++ svelte-sveltekit/svelte-learning/src/routes/+page.svelte
@@ -5,3 +5,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <p><a href={resolve('/state-class')}>$state 用在 class 里的验证页面</a></p>
+<p><a href={resolve('/template-syntax/element-attributes')}>元素属性花括号表达式验证页面</a></p>
